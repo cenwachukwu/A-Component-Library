6f224700be2bfa56bd841193106d2dc1ae77a6b4
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-11-01 09:18:38 -0400

    changes made

--- src/components/Forms/Form.stories.js
+++ src/components/Forms/Form.stories.js
@@ -1,13 +1,82 @@
 import React from "react";
 import { storiesOf } from '@storybook/react';
 import Forms from './Forms';
-// import './Forms.css';
+import './Forms.css';
 
+// storiesOf('Forms', module)
+//     .add('Forms', () => <input label="Email" />)
 storiesOf('Forms', module)
-    .add('Forms', () => <input label="Email" />)
-storiesOf('Forms', module)
-    .add('Primary', () => <input
+    .add('Primary Small', () => <Forms
         placeholder="Email"
         label="Email"
         type="text"
+        inputType="email"
+        small
+
     />)
+    .add('Primary Regular', () => <Forms
+        placeholder="Email"
+        label="Email"
+        type="text"
+        inputType="email"
+        regular
+    />)
+    .add('Primary Large', () => <Forms
+        placeholder="Email"
+        label="Email"
+        type="text"
+        inputType="email"
+        large    
+    />)
+
+    .add('Select',() => <Forms
+        // placeholder="select"
+        label="select"
+        type="select"
+        inputType="select"
+    />)
+    .add('Select fill',() => <Forms
+        placeholder="select"
+        label="select"
+        type="select"
+        inputType="select"
+    />)
+
+    .add('Voucher',() => <Forms
+        placeholder="Vouchercode"
+        label="Voucher code"
+        type="text"
+        inputType="submit"
+        
+    />)
+    .add('Voucher',() => <Forms
+        placeholder="Vouchercode"
+        label="Voucher code"
+        type="text"
+        inputType="submit"
+        
+    />)
+
+    .add('Checkbox',() => <Forms
+        placeholder=""
+        type="checkbox"
+        inputType="checkbox"
+    />)
+    .add('Checkbox blue',() => <Forms
+        placeholder=""
+        type="checkbox"
+        inputType="checkbox"
+    />)
+    .add('Checkbox text',() => <Forms   //with popups
+        placeholder=""
+        type="checkbox"
+        inputType="checkbox"
+    />)
+    .add('Counter', () => <Forms
+        type = "count"
+        inputType = "counter"
+        step = "5"
+    />)
+
+
+
---
+++ storybook-static/precache-manifest.6e1898e3d144b70751915afefb34b849.js
@@ -0,0 +1,31 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d531b52c31b219a363c2d876101c7838",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ac4167d0969811203ccd.bundle.js"
+  },
+  {
+    "revision": "3a254d98e9806face5c1",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.ac4167d0969811203ccd.bundle.js"
+  },
+  {
+    "revision": "c307014ca46b79ed267faad122a63820",
+    "url": "static/media/App.test.c307014c.png"
+  },
+  {
+    "revision": "8655819c918b35e17debde254bc9da15",
+    "url": "static/media/cart_2.8655819c.png"
+  },
+  {
+    "revision": "c307014ca46b79ed267faad122a63820",
+    "url": "static/media/hear.c307014c.png"
+  },
+  {
+    "url": "vendors~main.ac4167d0969811203ccd.bundle.js"
+  }
+]);
